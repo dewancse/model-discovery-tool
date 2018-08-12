13c7e33a52c88e1ddab4e971a1c33277ef4e3983
Dewan Sarwar <sarwarcse@gmail.com>
2018-08-12 12:51:49 +1200

    deployed version to heroku cloud

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
-'use strict';
-
-// Constants
-var PORT = 8080;
-var HOST = '127.0.0.1';
+var PORT = process.env.PORT || 8080;
 
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.text());
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-app.use('/', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * API
- */
-app.get('/api', function (req, res, next) {
-    var data = {
-        message: 'Hello World!'
-    };
-    res.status(200).send(data);
+app.get('/', function (req, res) {
+    res.render('public/index');
 });
-
-app.get('/server.html', function (req, res, next) {
-    var data = {
-        message: 'Hello World!'
-    };
-    res.status(200).send(data);
-});
-
-module.exports = app;
 
 app.listen(PORT, function () {
-    console.log('Express server listening on port ' + PORT);
+    console.log('Server running at http://127.0.0.1:' + PORT + '/');
 });
